1998ea4c53f4c8285a0ccf803fc92be5c5854333
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2026-04-02 11:16:20 -0600

    Replace uuid for native crypto function

--- app/components/PhoneMockup.tsx
+++ app/components/PhoneMockup.tsx
@@ -8,11 +8,7 @@ import { type Message } from "../lib/data";
 const SESSION_COOKIE_NAME = "aidenta_session";
 
 function generateUUID(): string {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+  return crypto.randomUUID()
 }
 
 function getSessionId(): string {
